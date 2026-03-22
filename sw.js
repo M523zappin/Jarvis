ba50c3839b18d295c4000fde3de455622a011029
M523zappin <robertvann223@gmail.com>
2026-03-22 10:32:59 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,38 @@
-const cacheName = 'ka-a2a-v1';
-const assets = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = 'ka-a2a-genesis-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js'
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+// Installation: Prime the Neural Cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[Sentinel] Priming Neural Cache...');
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+// Activation: Clean old logic patches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch: Ghost Pathing through cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
