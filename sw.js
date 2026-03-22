60e9fd84b9a8144823ddb42fd21c7200fb7c9ee5
M523zappin <robertvann223@gmail.com>
2026-03-22 11:09:08 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const VERSION = 'a2a-5.0-phantom';
+const CACHE = 'a2a-v5.1-interruption';
 self.addEventListener('install', e => {
   self.skipWaiting();
-  e.waitUntil(caches.open(VERSION).then(c => c.addAll(['./', './index.html', './manifest.json'])));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html', './manifest.json'])));
 });
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== VERSION).map(k => caches.delete(k)))));
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
 });
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
