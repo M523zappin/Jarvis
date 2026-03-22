2168db16e7336ff5e2aa2103888578223a0a21f7
M523zappin <robertvann223@gmail.com>
2026-03-22 10:55:54 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,15 @@
-const CACHE_NAME = 'ka-a2a-omni-v3.0';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js'
-];
+const CACHE_NAME = 'ka-a2a-v4.0';
+const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
-    })
-  );
+  e.waitUntil(caches.keys().then((keys) => {
+    return Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
+  }));
 });
 
 self.addEventListener('fetch', (e) => {
